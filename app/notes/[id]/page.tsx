3f6d7e8ce445cbb3f1da7b37162961a969c81a72
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-22 04:55:03 +0700

    ID Working

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,22 +1,19 @@
-async function getNote(username: string) {
+
+async function getNote(noteId: string) {
   const res = await fetch(
-    `http://127.0.0.1:8090/api/collections/profiles/records/${username}`,
+    `http://127.0.0.1:8090/api/collections/profiles/records/${noteId}`,
   );
   const data = await res.json();
   return data;
 }
 
 export default async function NotePage({ params }: any) {
-  const note = await getNote(params.username);
+  const note = await getNote(params.id);
 
   return (
     <div>
-      <h1>notes/{note.username}</h1>
-      <div>
+      <h1>notes/{note.id}</h1>
         <h3>{note.username}</h3>
-        <h5>{note.username}</h5>
-        <p>{note.display_name}</p>
-      </div>
     </div>
   );
 }
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,47 +2,45 @@
 import Link from 'next/link';
 
 
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('http://127.0.0.1:8090');
+// export const dynamic = 'auto',
+//   dynamicParams = true,
+//   revalidate = 0,
+//   fetchCache = 'auto',
+//   runtime = 'nodejs',
+//   preferredRegion = 'auto'
 
 
 async function getNotes() {
-  const db = new PocketBase('http://127.0.0.1:8090');
-  const result = await db.records.getList('profiles');
+  // const db = new PocketBase('http://127.0.0.1:8090');
+  // const result = await db.records.getList('notes');
   const res = await fetch('http://127.0.0.1:8090/api/collections/profiles/records', { cache: 'no-store' });
   const data = await res.json();
   return data?.items as any[];
 }
 
-
-
 export default async function NotesPage() {
   const notes = await getNotes();
-
   return(
     <div>
       <h1>Notes</h1>
       <div>
         {notes?.map((note) => {
-          return <Note key={note.username} note={note} />;
+          return <Note key={note.id} note={note} />;
         })}
       </div>
- 
+
     </div>
   );
 }
 
-
-
 function Note({ note }: any) {
-  const { username, display_name } = note || {};
+  const { id, username } = note || {};
 
   return (
-    <Link href={`/notes/${username}`}>
-      <div>
-        <h2>{display_name}</h2>
-        <h5>{username}</h5>
+    <Link href={`/notes/${id}`}>
+      <div >
+        <h2>{username}</h2>
+     
       </div>
     </Link>
   );
